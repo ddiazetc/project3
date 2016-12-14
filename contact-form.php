091eb0358258a9e1689005b13aee669a9daaaa56
ddiazetc <ddiazetc@u.rochester.edu>
2016-12-14 14:43:43 -0500

    created contact form

--- contact-form.php
+++ contact-form.php
@@ -1,14 +1,24 @@
-<!doctype html>
+<!-- <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1">
+
+    <title>Contact me</title>
+    
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="images/favicon.ico">
+
+    <?php include 'inc/fonts.inc' ?>
+
+    <link rel="stylesheet" href="css/fonts.css">
 </head>
 
 <body>
-	<h1>Contact</h1>
+	<h1>Contact</h1> -->
 	<!-- <form action="database-write.php" name="myForm" onsubmit="return(validate());"> -->
 
-	<form action="database-write.php" method="post">
+	<!-- <form action="database-write.php" method="post">
 	<div>
 		<label for="firstname">First name</label>
 		<input type="text" name="firstname" />
@@ -23,15 +33,15 @@
 		<label for="phone">Telephone</label>
 		<input type="text" name="phone" />
 		<br>
-	</div>
+	</div> -->
 <!-- 	<div>
 		<label for="msg_input">msg input</label>
 		<textarea name="msg_input" form="myForm"></textarea>
 		<br>
 	</div> -->
-	<input type="submit" value="Send">
+<!-- 	<input type="submit" value="Send">
 	
-	</form>
+	</form> -->
 <!-- 	<form action="database-write.php" name="myForm" method="post">
 		<h2>Name:</h2>
 		<input type="text" name="Name_input" />
@@ -85,5 +95,82 @@
 		return (true);
 	}
 </script> -->
+<!-- </body>
+</html> -->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1">
+
+    <title>Contact me</title>
+    
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="images/favicon.ico">
+
+    <?php include 'inc/fonts.inc' ?>
+
+    <link rel="stylesheet" href="css/fonts.css">
+</head>
+
+<body>
+<div class="cover">
+
+<?php include 'inc/nav.inc' ?>
+
+<div class="container">
+    <section>
+        <h2>Contact</h2>
+	<!-- <form action="database-write.php" name="myForm" onsubmit="return(validate());"> -->
+
+		<form action="database-write.php" name="myForm" method="post">
+			<div>
+				<label for="firstname">First name</label>
+				<input type="text" name="firstname" />
+				<br>
+			</div>
+			<div>
+				<label for="lastname">Last name</label>
+				<input type="text" name="lastname" />
+				<br>
+			</div>
+			<div>
+				<label for="phone">Telephone</label>
+				<input type="text" name="phone" />
+				<br>
+			</div>
+		<!-- 	<div>
+				<label for="msg_input">msg input</label>
+				<textarea name="msg_input" form="myForm"></textarea>
+				<br>
+			</div> -->
+			<input type="submit" value="Send">
+		</form>
+    </section>
+
+    <?php include 'inc/footer.inc' ?>
+
+</div><!--container-->
+</div><!--cover-->
+
+<!--JQuery-->
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+
+<script>document.getElementById("mobile-nav").style.opacity = "0";</script> <!--For some reason the opacity isn't 0 immediately.-->
+<script src="js/open-nav.js"></script>
+
+<!--Smooth scroll-->
+<script src="js/smoothscroll.js"></script>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-76799908-3', 'auto');
+    ga('send', 'pageview');
+</script>
 </body>
-</html>
+</html>              
